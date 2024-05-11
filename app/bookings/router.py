97674e9992a2650d3fe1c356327bf3e346488143
pantyukhinda <dmitriy.pantyukhin@mail.ru>
2024-05-11 13:46:13 +0500

    dao pattern

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from sqlalchemy import select
-from app.bookings.models import Bookings
-from app.database import async_session_maker
+
+from app.bookings.dao import BookingDAO
 
 
 router = APIRouter(
@@ -12,7 +11,4 @@ router = APIRouter(
 
 @router.get("")
 async def get_bookings():
-    async with async_session_maker() as session:
-        query = select(Bookings)
-        result = await session.execute(query)
-        return result.mappings().all()
+    return await BookingDAO.find_all()
---
+++ app/dao/base.py
@@ -0,0 +1,14 @@
+from app.database import async_session_maker
+from app.bookings.models import Bookings
+from sqlalchemy import select
+
+
+class BaseDAO:
+    model = None
+
+    @classmethod
+    async def find_all(cls):
+        async with async_session_maker() as session:
+            query = select(cls.model)
+            result = await session.execute(query)
+            return result.scalars().all()
