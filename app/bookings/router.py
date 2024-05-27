45965cb91cbf9ac7dd71015a121efe96e326c040
Пантюхин Дмитрий Анатольевич <pantyukhinda@metafrax.ru>
2024-05-27 17:20:25 +0500

    auth with user_id

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
 
 from app.bookings.dao import BookingDAO
 from app.bookings.shemas import SBooking
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 
 router = APIRouter(
@@ -11,5 +13,5 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings(user):  # -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)):  # -> list[SBooking]:
+    return await BookingDAO.find_all(user_id=1)
