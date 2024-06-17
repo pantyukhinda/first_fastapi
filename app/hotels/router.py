a887113d0d1f88ac98b4cae7366f78a749229a19
pantyukhinda <dmitriy.pantyukhin@mail.ru>
2024-06-17 14:48:10 +0500

    start celery

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,5 +1,8 @@
-from datetime import date
-from fastapi import APIRouter
+import asyncio
+from datetime import date, datetime
+from fastapi import APIRouter, Query
+from fastapi_cache.decorator import cache
+from pydantic import TypeAdapter
 
 from app.hotels.dao import HotelDAO
 from app.hotels.shemas import SHotel, SHotels
@@ -14,18 +17,22 @@ router = APIRouter(
 
 # возвращает список отелей по заданным параметрам, причем в отеле должен быть
 # минимум 1 свободный номер
-@router.get("/free")
+@router.get("/{location}")
+@cache(expire=60)
 async def get_hotels(
-    date_from: date,
-    date_to: date,
+    date_from: date = Query(..., description=f"Например {datetime.now().date()}"),
+    date_to: date = Query(..., description=f"Например {datetime.now().date()}"),
     location: str = "",
-) -> list[SHotels]:
+):
 
+    await asyncio.sleep(3)
     free_hotels = await HotelDAO.find_all(location, date_from, date_to)
+    hotels_list_adapter = TypeAdapter(list[SHotels])
+    free_hotels_json = hotels_list_adapter.validate_python(free_hotels)
     if not free_hotels:
         raise NoHotelsAvailableException
 
-    return free_hotels
+    return free_hotels_json
 
 
 @router.get("/id/{id}")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from posixpath import abspath, dirname
 import sys
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
 from typing import Optional
 from datetime import date
 from fastapi.staticfiles import StaticFiles
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+from contextlib import asynccontextmanager
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
@@ -15,9 +21,24 @@ from app.pages.router import router as router_pages
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 from app.images.router import router as router_images
+from app.config import settings
 
 
-application = FastAPI()
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    # при запуске
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        encoding="utf8",
+        decode_responses=True,
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
+    yield
+    # при выключении
+
+
+application = FastAPI(lifespan=lifespan)
+
 
 application.mount("/static", StaticFiles(directory="app/static"), "static")
 
@@ -27,6 +48,24 @@ application.include_router(router_hotels)
 application.include_router(router_rooms)
 application.include_router(router_pages)
 application.include_router(router_images)
+
+origins = [
+    "http://localhost:8000",
+]
+
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
 
 
 class SHotel(BaseModel):
@@ -54,30 +93,6 @@ class SHotelsSearchArgs:
 @application.get("/")
 async def root():
     return {"message": "Hello, world!"}
-
-
-# @application.get("/hotels/{hotel_id}")
-# async def get_hotel(hotel_id: int, date_from, date_to):
-#     return {"hotel_id": hotel_id, "date_from": date_from, "date_to": date_to}
-
-
-# @application.get("/hotels")
-# async def get_hotels(
-#     location: str,
-#     date_from: date,
-#     date_to: date,
-#     has_spa: Optional[bool] = None,
-#     stars: Optional[int] = Query(None, ge=1, le=5),
-# ) -> list[SHotel]:
-#     hotels = [
-#         {
-#             "address": "ул. Гагарина, 1, Алтай",
-#             "name": "Super Hotel",
-#             "stars": 5,
-#         }
-#     ]
-
-#     return hotels
 
 
 # if __name__ == "__main__":
